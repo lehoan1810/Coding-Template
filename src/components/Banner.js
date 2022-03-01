7583ceb1f7b407249dcd0a28ab57c2222e7f4a58
puimekster <hoanhao18102000@gmail.com>
2022-03-02 01:59:19 +0700

    responsive topics

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -1,5 +1,9 @@
 import React from "react";
 import img2 from "../images/img3.jpg";
+import topics1 from "../images/topics1.png";
+import topics2 from "../images/topics2.png";
+import topics3 from "../images/topics3.png";
+import topics4 from "../images/topics4.png";
 
 const Banner = () => {
 	return (
@@ -24,14 +28,48 @@ const Banner = () => {
 				</div>
 			</section>
 			<section className="contagion">
-				<h2 className="contagion-title">Covid-19</h2>
-				<h1 className="contagion-title1">Contagion</h1>
-				<div className="block-desc">
-					<span className="contagion-desc">
-						Corona viruses are a type of virus. There are many different kinds,
-						and some cause disease. A newly identified type
-					</span>
+				<div className="contagion-center">
+					<h2 className="contagion-title">Covid-19</h2>
+					<h1 className="contagion-title1">Contagion</h1>
+					<div className="block-desc">
+						<span className="contagion-desc">
+							Corona viruses are a type of virus. There are many different
+							kinds, and some cause disease. A newly identified type
+						</span>
+					</div>
 				</div>
+				<div className="topics">
+					<div className="topics-item">
+						<img src={topics1} alt="" />
+						<div className="topics-content">
+							<h2 className="topics-title">Air Transmission</h2>
+							<span className="topics-desc">
+								Objectively evolve tactical expertise before extensible
+								initiatives. Efficiently simplify
+							</span>
+						</div>
+					</div>
+					<div className="topics-item">
+						<img src={topics2} alt="" />
+						<div className="topics-content">
+							<h2 className="topics-title">Human Contacts</h2>
+							<span className="topics-desc">
+								Washing your hands is one of thesimplest ways you can protect
+							</span>
+						</div>
+					</div>
+					<div className="topics-item">
+						<img src={topics3} alt="" />
+						<div className="topics-content">
+							<h2 className="topics-title">Containted Objects</h2>
+							<span className="topics-desc">
+								Use the tissue while sneezing,In this way, you can protect your
+								droplets.
+							</span>
+						</div>
+					</div>
+				</div>
+				<img className="topics-outside" src={topics4} alt="" />
 			</section>
 		</div>
 	);
